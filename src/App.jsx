03490d9ec9acfc455034ffb1ea5da2035ceb0091
saumyadip25 <saumyadipmandal551@gmail.com>
2023-10-06 00:00:50 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="main">
+      <p>
+        This is a React App created without conventional{" "}
+        <code>npx create-react-app</code>.
+      </p>
+    </div>
+  );
+}
